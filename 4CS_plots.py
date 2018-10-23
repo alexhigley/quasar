e01ca7f830f95f27ed0895ac51480d9cd94ddea7
alexhigley <alexhigley1999@gmail.com>
2018-10-23 12:01:22 -0600

    Fixed the axes, made a historgram, changed the tick spacing.

--- 4CS_plots.py
+++ 4CS_plots.py
@@ -1,3 +1,9 @@
+
+#######################################################################
+#                           4CS 2018 plots                            #
+#             Learning Object Oriented plotting on python             #
+#######################################################################
+
 import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
@@ -51,27 +57,44 @@ fig1,ax1 = plt.subplots(figsize=(10,8)) #for spec, 15x12
 #spec class.py on brad git
     #tick params
     #ax1, fig 1
-fig1,ax1 = plt.subplots(figsize=(10,8))
 
-ax1.scatter(unzar,diff,marker='.',color='black')
-#making scatter plot
+ax1.scatter(unzar,rel_diff,marker='.',color='black')
+    #ax1.set_yscale('log',nonposy='clip')
+#Making scatter plot of relative difference to z (VI)
 
-ax1.set_xlabel('Z_PIPE',fontsize=10)
-ax1.set_ylabel('Z_VI - Z_PIPE',fontsize=10)
-#labels
+ax1.set_xlabel(r'z$_{\textrm{p}}$',fontsize=25)
+ax1.set_ylabel(r'$\Delta$ z',fontsize=25)
+#Setting labels
+#Was messed up before because need to write in latex code
 
 ax1.tick_params(axis='both',direction='in')
 ax1.tick_params(axis='both',which='minor',direction='in')
 ax1.tick_params(top=True,right=True)
 ax1.tick_params(which='minor',top=True,right=True)
-#ticks, should point in
+#ticks pointing in
 
 ax1.xaxis.set_major_locator(ticker.MultipleLocator(1))
 ax1.yaxis.set_major_locator(ticker.MultipleLocator(1))
 ax1.xaxis.set_minor_locator(ticker.MultipleLocator(0.1))
-ax1.yaxis.set_minor_locator(ticker.MultipleLocator(0.05))
-#tick spacing
+ax1.yaxis.set_minor_locator(ticker.MultipleLocator(0.1))
+#spacing of both major and minor tickers
 
-plt.show()
+fig1.savefig('z_diff_scat.png',format='png')
+#saving first graph as a png
 
-#A lot of problems. Axies now showing up.
+fig2,ax2 = plt.subplots(figsize=(10,8))
+ax2.hist(rel_diff,bins=100,histtype='stepfilled',color='slategrey')
+#Making histogram of frequency vs relative diff
+
+ax2.tick_params(axis='both',direction='in')
+ax2.tick_params(axis='both',which='minor',direction='in')
+ax2.tick_params(top=True,right=True)
+ax2.tick_params(which='minor',top=True,right=True)
+#Setting up picks, pointing in
+
+ax2.set_xlabel(r'$\Delta$ z',fontsize=25)
+ax2.set_ylabel('Frequency',fontsize=25)
+#Labeling axes, again, in latex code
+
+fig2.savefig('freq_err_hist.png',format='png')
+#saving second graph as a png
